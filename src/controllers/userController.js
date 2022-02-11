5ef9611c6ccee37eeb479c657dc0e905340c9350
manaemee <78135473+manaemee@users.noreply.github.com>
2022-02-11 23:52:09 +0900

    form validation

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,7 +3,19 @@ import User from "../models/User";
 export const editUser = (req, res) => res.send("Edit User");
 export const getJoin = (req, res) => {res.render("join" , {pageTitle: "Join"})};
 export const postJoin = async (req, res) => {
-    const {name, username, email, password} = req.body;
+    const {name, username, email, password} = req.body; 
+    const exits = await User.exists({username, email});
+    if(exits){
+        return res.render("join", {pageTitle:"Join", errorMessage:"This username/e-mail are already taken"});
+    }
+    const exitsUsername = await User.exists({username});
+    if(exitsUsername){
+        return res.render("join", {pageTitle:"Join", errorMessage:"This username is already taken"})
+    };
+    const exitsEmail = await User.exists({email});
+    if(exitsEmail){
+        return res.render("join", {pageTitle:"Join", errorMessage:"This e-mail is already taken"});
+    }
     await User.create({
         name,
         username,
--- src/models/User.js
+++ src/models/User.js
@@ -3,15 +3,12 @@ import bcrypt from "bcrypt";
 
 const UserSchema = new mongoose.Schema({
     name: {type:String, required:true},
-    username:{type:String, required:true, unique:true, maxlength:5},
+    username:{type:String, required:true, unique:true, maxlength:8},
     email: {type:String, required:true, unique:true},
     password: {type:String, required:true, minlength:5},
 })
-UserSchema.pre("save", async function(){
-    this.password = this.password, 5, function(err, hash){
 
-    }
-})
+
 UserSchema.pre('save', async function() {
     this.password = bcrypt.hashSync(this.password, 5);
   });
