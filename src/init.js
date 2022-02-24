f16bd97ee5b5b61024d250c7df1b5e8770749c25
manaemee <78135473+manaemee@users.noreply.github.com>
2022-02-25 00:50:29 +0900

    PORT env variable

--- src/init.js
+++ src/init.js
@@ -5,5 +5,5 @@ import app from "./server";
 
 
 
-const PORT = 4000;    
+const PORT = process.env.PORT || 4000;    
 app.listen(4000, () => console.log(`Server listening on port http://localhost:${PORT}`));
